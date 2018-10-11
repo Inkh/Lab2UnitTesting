3c72b182bfd9751147bc3e0ddbf4941999839309
inkh <jhwe92@gmail.com>
2018-10-10 21:22:12 -0700

    Test written for withdrawal

--- ATM/Lab2-Test/UnitTest1.cs
+++ ATM/Lab2-Test/UnitTest1.cs
@@ -33,6 +33,22 @@ namespace Lab2_Test
             Assert.IsType<ArgumentException>(e);
         }
 
-        
+        [Fact]
+        public void WithdrawalWillBreak()
+        {
+            int balance = 5000;
+            string input = "hi i don't follow instructions";
+            Action breakWithdrawal = (() => Withdraw(input, balance));
+            Exception e = Record.Exception(breakWithdrawal);
+            Assert.IsType<FormatException>(e);
+        }
+
+        [Fact]
+        public void WillWithdraw()
+        {
+            int balance = 5000;
+            string input = "4000";
+            Assert.Equal(1000, Withdraw(input, balance));
+        }
     }
 }

e1168fe0902db8d744f3f747b6f4f8b2d1761fbc
inkh <jhwe92@gmail.com>
2018-10-10 20:32:25 -0700

    Refactored code, added finally block.

--- ATM/ATM/Program.cs
+++ ATM/ATM/Program.cs
@@ -12,9 +12,13 @@ namespace ATM
 
 
         }
-
+        /// <summary>
+        /// The greeting menu for ATM users. It will continuously bug the user to choose an option until they selected "Exit". 
+        /// </summary>
+        /// <param name="balance">Default starting balance</param>
         static void Greeting(int balance)
         {
+            //Outer while loop for users to come back to everytime they finish a transaction.
             bool restart = true;
             while (restart)
             {
@@ -23,125 +27,98 @@ namespace ATM
                 Console.WriteLine("[1] View Balance");
                 Console.WriteLine("[2]. Deposit Money");
                 Console.WriteLine("[3]. Withdraw Money");
-                Console.WriteLine("[4]. Exit");
-                bool flag = true;
-                while (flag)
+
+                try
                 {
-                    flag = false;
-                    try
+                    int userInput = int.Parse(Console.ReadLine());
+                    switch (userInput)
                     {
-                        int userInput = int.Parse(Console.ReadLine());
-                        switch (userInput)
-                        {
-                            case 1:
-                                Console.WriteLine($"You currently have ${balance}");
-                                Console.WriteLine("Would you like to perform another transaction? [Yes] / [No]");
-                                string userAction = Console.ReadLine();
-                                switch (userAction.ToLower())
+                        case 1:
+                            break;
+                        case 2:
+                            bool error = true;
+                            Console.WriteLine($"You currently have ${balance}");
+                            Console.WriteLine("How much would you like to deposit?");
+                            while (error)
+                            {
+                                error = false;
+                                try
                                 {
-                                    case "yes":
-                                        restart = true;
-                                        break;
-                                    case "no":
-                                        break;
-                                    default:
-                                        Console.WriteLine("Did not understand. Redirecting you back to menu...");
-                                        restart = true;
-                                        break;
+                                    balance = Deposit(Console.ReadLine(), balance);
                                 }
-                                break;
-                            case 2:
-                                bool error = true;
-                                Console.WriteLine($"You currently have ${balance}");
-                                Console.WriteLine("How much would you like to deposit?");
-                                while (error)
+                                catch (FormatException)
                                 {
-                                    error = false;
-                                    try
-                                    {
-                                        balance = Deposit(Console.ReadLine(), balance);
-                                    }
-                                    catch (FormatException)
-                                    {
-                                        Console.WriteLine("That was not a number. Please try again.");
-                                        error = true;
-                                    }
+                                    Console.WriteLine("That was not a number. Please try again: ");
+                                    error = true;
+                                }
+                            }
+
+
+                            break;
+                        case 3:
+                            error = true;
+                            Console.WriteLine($"You currently have ${balance}");
+                            Console.WriteLine("How much would you like to withdraw?");
+                            while (error)
+                            {
+                                error = false;
+                                try
+                                {
+                                    balance = Withdraw(Console.ReadLine(), balance);
+                                }
+                                catch (FormatException)
+                                {
+                                    Console.WriteLine("That was not a number. Please try again.");
+                                    error = true;
                                 }
 
-                                Console.WriteLine($"Your current balance is now {balance}");
-                                Console.WriteLine("Would you like to perform another transaction?");
-                                userAction = Console.ReadLine();
-                                switch (userAction.ToLower())
+                                catch (ArgumentException e)
                                 {
-                                    case "yes":
-                                        restart = true;
-                                        break;
-                                    case "no":
-                                        break;
-                                    default:
-                                        Console.WriteLine("Did not understand. Redirecting you back to menu...");
-                                        restart = true;
-                                        break;
+                                    Console.WriteLine(e.Message);
+                                    error = true;
                                 }
-                                break;
-                            case 3:
-                                error = true;
-                                Console.WriteLine($"You currently have ${balance}");
-                                Console.WriteLine("How much would you like to withdraw?");
-                                while (error)
-                                {
-                                    error = false;
-                                    try
-                                    {
-                                        balance = Withdraw(Console.ReadLine(), balance);
-                                    }
-                                    catch (FormatException)
-                                    {
-                                        Console.WriteLine("That was not a number. Please try again.");
-                                        error = true;
-                                    }
+                            }
 
-                                    catch (ArgumentException e)
-                                    {
-                                        Console.WriteLine(e.Message);
-                                        error = true;
-                                    }
-                                }
-                                Console.WriteLine($"Your current balance is now {balance}");
-                                Console.WriteLine("Would you like to perform another transaction? [Yes] / [No]");
-                                userAction = Console.ReadLine();
-                                switch (userAction.ToLower())
-                                {
-                                    case "yes":
-                                        restart = true;
-                                        break;
-                                    case "no":
-                                        break;
-                                    default:
-                                        Console.WriteLine("Did not understand. Redirecting you back to menu...");
-                                        restart = true;
-                                        break;
-                                }
-                                break;
-                            case 4:
-                                break;
-                            default:
-                                Console.WriteLine("Please input 1, 2, 3, or 4 for operations");
-                                flag = true;
-                                break;
+                            break;
+                        default:
+                            Console.WriteLine("Please input 1, 2, or 3 for operations");
+                            restart = true;
+                            break;
 
-                        }
                     }
-                    catch (FormatException)
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please input 1, 2, or 3 for operations");
+                    restart = true;
+                }
+                finally
+                {
+                    Console.WriteLine($"Your current balance is now {balance}");
+                    Console.WriteLine("Would you like to perform another transaction? [Yes] / [No]");
+                    string userAction = Console.ReadLine();
+                    switch (userAction.ToLower())
                     {
-                        Console.WriteLine("Please input 1, 2, 3, or 4 for operations");
-                        flag = true;
+                        case "yes":
+                            restart = true;
+                            break;
+                        case "no":
+                            break;
+                        default:
+                            Console.WriteLine("Did not understand. Redirecting you back to menu...");
+                            restart = true;
+                            break;
                     }
                 }
             }
            
         }
-
+        /// <summary>
+        /// Handles the deposit action of ATM. Will add to balance if format is valid. Otherwise, throw Exception to parent block.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="balance"></param>
+        /// <returns></returns>
         static int Deposit(string input, int balance)
         {
             try
@@ -155,6 +132,12 @@ namespace ATM
             return balance;
         }
 
+        /// <summary>
+        /// Performs the withdraw method of an ATM. Throws exception if format is incorrect. Also throws exception if withdrawal amount is greater than balance.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="balance"></param>
+        /// <returns></returns>
         static int Withdraw(string input, int balance)
         {
             try
